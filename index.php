9fcce74243396a499055310b834e1c10ee07c109
Bardark <ren_rbk@hotmail.es>
2017-09-07 18:02:00 -0500

    ya inserta y se le puso mas estilo

--- index.php
+++ index.php
@@ -12,52 +12,41 @@
     <!-- Font-Awesome -->
     <link type="text/css" rel="stylesheet" href="font-awesome/css/font-awesome.min.css"/>
     <link type="text/css" rel="stylesheet" href="font-awesome/css/font-awesome.css"/>
-
-    <!-- Para el campo de fecha -->
-    <link rel="stylesheet" href="css/jquery-ui.css" />
-    <script Language=Javascript src="js/jquery-1.12.4.js"></script>
-		<script Language=Javascript src="js/jquery-ui.js"></script>
-    <script Language=Javascript src="js/jquery.maskedinput.js"></script>
-    <script>
-		$( function() {
-			$( "#datepicker" ).datepicker();
-		} );
-  </script>
   </head>
 
   <body>
     <div class="container-fluid">
       <div class="Estilo1 row">
-        <form name="form" method="post" action="php/enviar.php">
+        <form role="form" name="form" method="post" action="php/enviar.php" enctype="multipart/form-data" id="formGen">
           <div class="col-md-12 span">
             <span class="Estilo1">Registro de facturas</span>
           </div>
           <div class="col-md-6">
             <label for="">Número de cliente</label>
-            <input name="numCliente" type="text" id="numCliente" required />
+            <input name="numCliente" type="text" id="numCliente" required placeholder="000001" autofocus="" />
           </div>
           <div class="col-md-6">
             <label for="">Nombre de cliente</label>
-            <input name="nomCliente" type="text" id="nomCliente" required />
+            <input name="nomCliente" type="text" id="nomCliente" required placeholder="Juan Peréz" />
           </div>
           <div class="col-md-6">
             <label for="">Factura</label>
-            <input name="nombreFac" type="text" id="nombreFac" required />
+            <input name="nombreFac" type="text" id="nombreFac" required placeholder="Nombre de la factura" />
           </div>
           <div class="col-md-6">
             <label for="">Monto antes de IVA</label>
-            <input name="importe" type="text" id="importe" required />
+            <input name="importe" type="text" id="importe" required placeholder="$123456.99" />
           </div>
           <div class="col-md-6">
             <label for="">Monto total</label>
-            <input name="importeTotal" type="text" id="importeTotal" required />
+            <input name="importeTotal" type="text" id="importeTotal" required placeholder="$123456.99" />
           </div>
           <div class="col-md-6">
             <label for="">Fecha</label>
-            <input name="datepicker" type="text" id="datepicker" required />
+            <input name="fechaPago" type="date" id="fechaPago" required />
           </div>
-          <div class="col-md-12">
-            <input id="enviar" type="submit" name="enviar" value="enviar">
+          <div class="col-md-12" id="divenviar">
+            <input id="enviar" type="submit" class="btn-primary" name="enviar" value="Enviar">
           </div>
         </form>
       </div>
--- php/enviar.php
+++ php/enviar.php
@@ -1,9 +1,3 @@
-<html lang="es">
-	<head>
-		<meta charset="utf-8">
-	</head>
-</html>
-
 <?php
 // Recibimos por POST los datos procedentes del formulario
 $numCliente = ($_POST["numCliente"]);
@@ -11,22 +5,20 @@ $nomCliente = ($_POST["nomCliente"]);
 $nombreFac = ($_POST["nombreFac"]);
 $importe = ($_POST["importe"]);
 $importeTotal = ($_POST["importeTotal"]);
-$datepicker = ($_POST["datepicker"]);
+$fechaPago = ($_POST["fechaPago"]);
 
 		// Abrimos la conexion a la base de datos
-		$link = mysql_connect("localhost","root","");
+		mysql_connect("localhost","root","");
 		mysql_select_db("facturas") OR DIE ("Error: No es posible establecer la conexión");
-		$tildes = $link->query("SET NAMES 'utf8'");
-		mysql_query($link, "INSERT INTO registro_fac (numCliente, nomCliente, nomFactura, importeSinIVA, importeTotal, fechaPago,) VALUES ('$numCliente', '$nomCliente', '$nombreFac', '$importe', '$importeTotal', '$datepicker')");
-		//mysql_query('INSERT INTO registro_fac (numCliente, nomCliente, nombreFactura, importeSinIVA, importeTotal, fechaPago,) VALUES ("'.$numCliente'", "'.$nomCliente'", "'.$nombreFac'", "'.$importe'", "'.$importeTotal'", "'.$datepicker'")');
+		mysql_query("INSERT INTO registro_fac (numCliente, nomCliente, nomFactura, importeSinIVA, importeTotal, fechaPago) VALUES ('$numCliente', '$nomCliente', '$nombreFac', '$importe', '$importeTotal', '$fechaPago')");
 
 		// Confirmamos que el registro ha sido insertado con exito
 		/*echo "<script>
 	       alert('Los datos han sido guardados con exito');
 		     location.href='../index.php'
 				 </script>";*/
-				 echo "
-		 					<p>Los datos han sido guardados con exito.</p>
-		 					<p><a href='../index.php'>VOLVER ATRÁS</a></p>
-		 		";
+		echo "
+			<p>Los datos han sido guardados con exito.</p>
+		 	<p><a href='../index.php'>VOLVER ATRÁS</a></p>
+		";
 ?>
